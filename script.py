f6fe0d13c86405cae5e9af9f71af185c47586ec0
Andrei Petrov <avl.petrov@gmail.com>
2020-10-19 11:25:34 +0300

    Learn python

--- script.py
+++ script.py
@@ -4,3 +4,15 @@
 Copyright 2020 Andrei. """
 
 print("." * 10)
+
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+tabby_cat = "\t* I'm tabbed in."
+
+print(tabby_cat)
+
+print("How old are you?", end=' ')
+
+age = input()
+
+print(f"So, you're {age} old")
