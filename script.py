430f7b8329cce934b2997200fee93133b8e32178
Andrei Petrov <avl.petrov@gmail.com>
2020-10-20 12:01:04 +0300

    files

--- script.py
+++ script.py
@@ -14,3 +14,34 @@ print(f"Say goodbuy to {filename}.")
 
 target.truncate()
 
+print("I'm goint to ask you for three lines")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print(f"Now I'm adding these lines to the {filename}")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+
+print("And finally, we close it.")
+target.close()
+
+target = open(filename, 'r')
+
+for line in target:
+    print(line)
+
+target.close()
+
+
+
+
+
+
+
+
