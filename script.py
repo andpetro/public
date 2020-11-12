6e36b31258f8ee92992cd20195fec1e4388ce1b9
Andrei Petrov <avl.petrov@gmail.com>
2020-11-12 10:58:18 +0300

    test

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 # import pandas
 # import json
 
@@ -9,48 +16,48 @@
 
 # print(y)
 
-"""XLS -> json converter
-first:
-  $ pip install xlrd
-then:
-  $ cat in.xls
-date, temp, pressure
-Jan 1, 73, 455
-Jan 3, 72, 344
-Jan 7, 52, 100
-convert:
-  $ python xls_to_json.py in.xls Sheet1 out.json
-finally:
-  $ cat out.json
-{
-  'data': [
-    {'date': 'Jan 1', 'temp': 73, 'pressure': 455},
-    {'date': 'Jan 3', 'temp': 72, 'pressure': 344},
-    {'date': 'Jan 7', 'temp': 52, 'pressure': 100},
-  ]
-}
-"""
-import json
-import sys
+# """XLS -> json converter
+# first:
+#   $ pip install xlrd
+# then:
+#   $ cat in.xls
+# date, temp, pressure
+# Jan 1, 73, 455
+# Jan 3, 72, 344
+# Jan 7, 52, 100
+# convert:
+#   $ python xls_to_json.py in.xls Sheet1 out.json
+# finally:
+#   $ cat out.json
+# {
+#   'data': [
+#     {'date': 'Jan 1', 'temp': 73, 'pressure': 455},
+#     {'date': 'Jan 3', 'temp': 72, 'pressure': 344},
+#     {'date': 'Jan 7', 'temp': 52, 'pressure': 100},
+#   ]
+# }
+# """
+# import json
+# import sys
 
-import xlrd
+# import xlrd
 
 
-workbook = xlrd.open_workbook('./DSL_Profile.xlsx')
-worksheet = workbook.sheet_by_name('ADSL_AxJ_Profile_für_BNG')
+# workbook = xlrd.open_workbook('./DSL_Profile.xlsx')
+# worksheet = workbook.sheet_by_name('ADSL_AxJ_Profile_für_BNG')
 
-data = []
-keys = [v.value for v in worksheet.row(0)]
-for row_number in range(worksheet.nrows):
-    if row_number == 0:
-        continue
-    row_data = {}
-    for col_number, cell in enumerate(worksheet.row(row_number)):
-        row_data[keys[col_number]] = cell.value
-    data.append(row_data)
+# data = []
+# keys = [v.value for v in worksheet.row(0)]
+# for row_number in range(worksheet.nrows):
+#     if row_number == 0:
+#         continue
+#     row_data = {}
+#     for col_number, cell in enumerate(worksheet.row(row_number)):
+#         row_data[keys[col_number]] = cell.value
+#     data.append(row_data)
 
-with open('sample.json', 'w') as json_file:
-    json_file.write(json.dumps({'data': data}))
+# with open('sample.json', 'w') as json_file:
+#     json_file.write(json.dumps({'data': data}))
 
 
 
