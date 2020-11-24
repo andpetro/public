d7362ee7702d39b9248e54ab766ce4807c8ab0a3
Andrei Petrov <avl.petrov@gmail.com>
2020-11-24 10:12:41 +0300

    test

--- script.py
+++ script.py
@@ -1,21 +1,121 @@
-i = 0
-m = 99
-n = 5
-numbers =[]
 
-for i in range (0, m, n):
-  print(f"At the top i is {i}")
-  numbers.append(i)
+from sys import exit
 
-  #i = i + 1
-  print("Numbers now: ", numbers)
+def fuck_room():
+  print("You will be fucked")
+  print("Choose the method")
 
-  print(f"At the bottom i is {i}")
+  choice = input("> ")
 
-print("The numbers: ")
+  print(f"You are being fucked with {choice}")
 
-for num in numbers:
-  print(num)
+
+def gold_room():
+  print("This room is full of gold. How much do you take?")
+
+  choice = input("> ")
+  if "0" in choice or "1" in choice:
+    how_much = int(choice)
+  else:
+    dead("Man learn to type a number")
+  
+  if how_much < 50:
+    print("Nice you're not greedy. you win!")
+    exit(0)
+  else:
+    dead("You greedy bastard!")
+
+
+def bear_room():
+  print("There is a bear here")
+  print("The bear has a bunch of honey")
+  print("The fat bear is in front of other door")
+  print("Gow are you going to move the bear?")
+  bear_moved = False
+
+  while True:
+    choice = input("> ")
+
+    if choice == "take honey":
+      dead("The bear looks at you then slaps your face off.")
+    elif choice == "taunt bear" and not bear_moved:
+      print("The bear has moved from the door")
+      print("You can go thought it now")
+      bear_moved = True
+    elif choice == "taunt bear" and bear_moved:
+      dead("The bear gets pissed off and chews your leg off.")
+    elif choice == "open door" and bear_moved:
+      gold_room()
+    else:
+      print("I got no idea what that means")
+
+
+def cthulhu_room():
+  print("Here you see the great evil Cthulhu.")
+  print("He, it, whatever stares at you and you go insane.")
+  print("Do you flee for your life or eat your head?")
+
+  choice = input("> ")
+
+  if "flee" in choice:
+    start()
+  elif "head" in choice:
+    dead("Well that was tasty")
+  else:
+    cthulhu_room()
+
+def dead(why):
+  print(why, "Good job!")
+  exit(0)
+
+def start():
+  print("You are in a dark room.")
+  print("There is a door to your right and left.")
+  print("You can also go up or down")
+  print("Which one do you take?")
+
+  choice = input("> ")
+
+  if choice == "left":
+    bear_room()
+  elif choice == "right":
+    cthulhu_room()
+  elif choice == "up":
+    fuck_room()
+#  elif choice == "down":
+#    nt_room()
+  else:
+    dead("You stumble around the room until you starve.")
+
+start()
+
+
+
+
+
+
+  
+
+ 
+ 
+# i = 0
+# m = 99
+# n = 5
+# numbers =[]
+
+# for i in range (0, m, n):
+#   print(f"At the top i is {i}")
+#   numbers.append(i)
+
+#   #i = i + 1
+#   print("Numbers now: ", numbers)
+
+#   print(f"At the bottom i is {i}")
+
+# print("The numbers: ")
+
+# for num in numbers:
+#   print(num)
 
 
 
